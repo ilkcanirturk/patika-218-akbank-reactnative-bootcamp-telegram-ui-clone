aa0042efdf9bc3c608c82e0abb370e2953dc1aa4
kodluyoruz <info@kodluyoruz.org>
2022-09-02 21:46:17 +0300

    add StyleSheet and comments

--- TelegramClone/src/screens/ThemeScreen.js
+++ TelegramClone/src/screens/ThemeScreen.js
@@ -1,19 +1,60 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 import Octicons from 'react-native-vector-icons/Octicons';
 
 const ThemeScreen = () => {
   return (
-    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', marginTop: 150}}>
-      <TouchableOpacity style= {{ display: 'flex', height: 70, width: 100, backgroundColor: 'white', borderRadius: 25, borderColor: '#2AABEE', borderWidth: 2}}>
-        <Text style={{ display: 'flex', alignSelf:'center', marginTop: 13, color: '#229ED9', fontSize: 28}}>Light</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style= {styles.buttonLight}>
+        <Text style={styles.textLight}>Light</Text>
       </TouchableOpacity>
       <Octicons name= "arrow-switch" size= {70}/>
-      <TouchableOpacity style= {{ display: 'flex', height: 70, width: 100, backgroundColor: '#403d3d', borderRadius: 25, borderColor: '#2AABEE', borderWidth: 2}}>
-        <Text style={{ display: 'flex', alignSelf:'center', marginTop: 13, color: 'white', fontSize: 28}}>Dark</Text>
+      <TouchableOpacity style= {styles.buttonDark}>
+        <Text style={styles.textDark}>Dark</Text>
       </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex', 
+    flexDirection: 'row', 
+    justifyContent: 'space-around', 
+    marginTop: 150
+  },
+  buttonLight: {
+    display: 'flex', 
+    height: 70, 
+    width: 100, 
+    backgroundColor: 'white', 
+    borderRadius: 25, 
+    borderColor: '#2AABEE', 
+    borderWidth: 2
+  },
+  textLight: {
+    display: 'flex', 
+    alignSelf:'center', 
+    marginTop: 13, 
+    color: '#229ED9', 
+    fontSize: 28
+  },
+  buttonDark: {
+    display: 'flex', 
+    height: 70, 
+    width: 100, 
+    backgroundColor: '#403d3d', 
+    borderRadius: 25, 
+    borderColor: '#2AABEE', 
+    borderWidth: 2
+  },
+  textDark: {
+    display: 'flex', 
+    alignSelf:'center', 
+    marginTop: 13, 
+    color: 'white', 
+    fontSize: 28
+  }
+})
 
 export default ThemeScreen
