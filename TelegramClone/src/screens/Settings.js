94dc1dc4a16d91c3e9c897a9b3c38e188dbebf51
kodluyoruz <info@kodluyoruz.org>
2022-09-02 20:23:14 +0300

    add Settings Screen

---
+++ TelegramClone/src/screens/Settings.js
@@ -0,0 +1,70 @@
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+const Settings = () => {
+  const { navigate } = useNavigation();
+  return (
+    <View>
+      <Image style={styles.imageStyle} source={{uri: 'https://picsum.photos/id/231/200/300'}}/>
+      <Text style={styles.textName}>İlkcan</Text>
+      <View style= {styles.containerInfo}>
+        <Text style= {styles.textInfo}>
+            +90 507 442 1468
+        </Text>
+        <Text style= {styles.textInfo}>
+            •
+        </Text>
+        <Text style= {styles.textInfo}>
+            @WitcherMaster
+        </Text>
+      </View>
+      <Pressable onPress={() => navigate('Home')} style={styles.logoutButtonStyle}>
+        <Text style={styles.logoutTextStyle}>Log Out</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+export default Settings
+
+const styles = StyleSheet.create({
+    imageStyle: {
+        backgroundColor: 'gray', 
+        height: 120, 
+        width: 120, 
+        borderRadius: 75, 
+        marginVertical: 20, 
+        alignSelf: 'center'
+    },
+    textName: {
+        fontSize: 26, 
+        fontWeight: 'bold', 
+        color: 'black', 
+        alignSelf: 'center'
+    },
+    containerInfo: {
+        flexDirection: 'row', 
+        justifyContent: 'space-around', 
+        marginHorizontal: 55,
+        marginTop: 5
+    },
+    textInfo: {
+        fontSize: 16, 
+        color: 'gray'
+    },
+    logoutButtonStyle: {
+      width: 70, 
+      height: 40, 
+      backgroundColor: '#229ED9', 
+      justifyContent: 'center', 
+      alignSelf: 'center', 
+      marginTop: 250, 
+      borderRadius: 25
+    },
+    logoutTextStyle: {
+      justifyContent: 'center', 
+      alignSelf: 'center', 
+      color: 'white'
+    }
+})
