d404ba560372ee8eb99861dc4fe95d13bc947d65
kodluyoruz <info@kodluyoruz.org>
2022-09-02 20:19:48 +0300

    add Chat component

---
+++ TelegramClone/src/screens/Chat.js
@@ -0,0 +1,31 @@
+import {View, StyleSheet} from 'react-native';
+import React from 'react';
+import ChatHeader from '../components/ChatHeader';
+import ChatInput from '../components/ChatInput';
+import ChatCard from '../components/ChatCard';
+
+const Chat = ({route}) => {
+  const {firstName, pp, textData} = route.params;
+
+  return (
+    <View
+      style={styles.container}>
+      <View>
+        <ChatHeader firstName={firstName} photo={pp} />
+        <ChatCard textData={textData} />
+      </View>
+      <View>
+        <ChatInput />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: 'space-between'
+  },
+})
+
+export default Chat;
